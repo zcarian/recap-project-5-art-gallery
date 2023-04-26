2d7528af635dc1d1bc5c47870304ce33434a1924
zcarian <mar.jed.00@gmail.com>
2023-04-26 16:30:19 +0200

    cleaning

--- components/ArtPieces/index.js
+++ components/ArtPieces/index.js
@@ -4,7 +4,12 @@ export default function ArtPieces({pieces}) {
     return (
         <ul>
             {pieces.map((piece)=>{
-                return <li key={piece.slug}><ArtPiecePreview name={piece.name} image={piece.imageSource} artist={piece.artist}/></li>
+                return <li key={piece.slug}>
+                    <ArtPiecePreview 
+                        name={piece.name} 
+                        image={piece.imageSource} 
+                        artist={piece.artist}/>
+                </li>
             })}
         </ul>
     )
