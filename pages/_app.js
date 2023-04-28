1a098731a03877980050ada152dd3e4a51dc7c85
zcarian <mar.jed.00@gmail.com>
2023-04-28 15:12:19 +0200

    adding data to useEffect depedecies

--- pages/_app.js
+++ pages/_app.js
@@ -25,7 +25,7 @@ export default function App({ Component, pageProps }) {
         })
       )
     }
-  },[artPiecesInfo])
+  },[artPiecesInfo, data])
 
   function handleToggleFavorite(slug){
     setArtPiecesInfo(
