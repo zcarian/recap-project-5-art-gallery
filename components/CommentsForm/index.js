64621383c197e4e7ee83544e3f18fcc8784affcb
zcarian <mar.jed.00@gmail.com>
2023-04-28 14:55:33 +0200

    deleting console log

--- components/CommentsForm/index.js
+++ components/CommentsForm/index.js
@@ -12,7 +12,6 @@ export default function CommentForm({ onFormSubmit, slug }) {
         if (comment) {
             onFormSubmit(slug, comment);
             setComment("");
-            console.log(comment);
         }
     };
 
